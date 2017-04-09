a0b4ddca8ffc8a8a60c7878029256bd669c1e76a
Thomas Reto Strub <ths@strubt.ch>
2017-04-09 23:12:33 +0200

    use nMQTT

---
+++ MqttTest/Converters.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using Nmqtt;
+
+namespace StrubT.IoT.Mqtt.Test {
+
+	abstract class PublishDataConverter<T> : IPublishDataConverter {
+
+		public Func<T, byte[]> Encoder { get; }
+
+		public Func<byte[], T> Decoder { get; }
+
+		protected PublishDataConverter(Func<T, byte[]> encoder, Func<byte[], T> decoder) {
+
+			Encoder = encoder;
+			Decoder = decoder;
+		}
+
+		public byte[] Encode(T data) => Encoder(data);
+
+		public T Decode(byte[] data) => Decoder(data);
+
+		byte[] IPublishDataConverter.ConvertToBytes(object data) => data is T t ? Encode(t) : throw new ArgumentException($"Invalid type. Can only handle '{typeof(T)}'.", nameof(data));
+
+		object IPublishDataConverter.ConvertFromBytes(byte[] messageData) => Decode(messageData);
+	}
+
+	class DoubleConverter : PublishDataConverter<double> {
+
+		public static Encoding Encoding { get; } = Encoding.ASCII;
+
+		public DoubleConverter() : base(d => Encoding.GetBytes(d.ToString()),
+			b => double.Parse(Encoding.GetString(b))) { }
+	}
+}
--- MqttTest/Program.cs
+++ MqttTest/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Text;
-using uPLibrary.Networking.M2Mqtt;
-using uPLibrary.Networking.M2Mqtt.Messages;
+using Nmqtt;
 
 namespace StrubT.IoT.Mqtt.Test {
 
@@ -9,23 +7,21 @@ namespace StrubT.IoT.Mqtt.Test {
 
 		static void Main() {
 
-			var centerGuid = "F386-09CA-1F9F-9A1F-BAD3-F573-64A0-2A22";
-			var sensorGuid = "52AF61E9-CDE0-480F-A7C4-7E0F01D23C93";
-			var clientGuid = Guid.NewGuid();
+			var topic = "siot/DAT/F386-09CA-1F9F-9A1F-BAD3-F573-64A0-2A22/52AF61E9-CDE0-480F-A7C4-7E0F01D23C93";
+			var clientId = "StrubT";
 
-			var client = new MqttClient("siot.net");
-			client.MqttMsgPublishReceived += (sender, e) => Console.WriteLine($"{double.Parse(Encoding.ASCII.GetString(e.Message)):0.00}");
+			using (var client = new MqttClient("siot.net", clientId)) {
+				client.MessageAvailable += (sender, e) => Console.WriteLine($"{e.Message:0.00}");
 
-			client.Connect(clientGuid.ToString());
-			client.Subscribe(new[] { $"siot/DAT/{centerGuid}/#" }, new[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+				client.Connect();
+				client.Subscribe<DoubleConverter>(topic, MqttQos.AtLeastOnce);
 
-			Console.ReadLine();
+				Console.ReadLine();
 
-			var r = new Random();
-			for (var i = 0; i < 5; i++)
-				client.Publish($"siot/DAT/{centerGuid}/{sensorGuid}", Encoding.ASCII.GetBytes((r.NextDouble() * 50 + 50).ToString()));
-
-			client.Disconnect();
+				var r = new Random();
+				for (var i = 0; i < 5; i++)
+					client.PublishMessage<DoubleConverter>(topic, r.NextDouble() * 50 + 50);
+			}
 		}
 	}
 }
